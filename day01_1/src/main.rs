ec9fe81b819373a0eaabdc5ce3ed03dbc6ab9d7b
Marat Bukharov <marat.buharov@gmail.com>
2024-12-02 00:45:36 +0300

    day 01 part 2

--- day01_1/src/main.rs
+++ day01_1/src/main.rs
@@ -1,59 +1,30 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::iter;
-use std::path::Path;
+use day01_lib::{input, load_input};
+use std::collections::HashMap;
 
-fn parse_line(line: &str) -> Option<(u32, u32)> {
-    let mut it = line.split_whitespace();
-    Some((it.next()?.parse().ok()?, it.next()?.parse().ok()?))
-}
-
-fn load_input(path: &Path) -> (Vec<u32>, Vec<u32>) {
-    let file = BufReader::new(File::open(path).unwrap());
-    file.lines()
-        .map_while(Result::ok)
-        .filter(|line| !line.is_empty())
-        .flat_map(|line| parse_line(line.as_str()))
-        .collect()
-}
-
-fn total_distance(mut a: Vec<u32>, mut b: Vec<u32>) -> u32 {
-    a.sort_unstable();
-    b.sort_unstable();
-    iter::zip(a, b).map(|(a, b)| a.abs_diff(b)).sum()
+fn similarity_score(a: &[u32], b: &[u32]) -> u32 {
+    let b_occurrences = b.iter().fold(HashMap::new(), |mut acc, x| {
+        acc.entry(x).and_modify(|x| *x += 1).or_insert(1);
+        acc
+    });
+    a.iter()
+        .map(|a| a * b_occurrences.get(&a).unwrap_or(&0))
+        .sum()
 }
 
 fn main() {
-    let (a, b) = load_input(Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/input.txt")));
-    println!("{}", total_distance(a, b));
+    let (a, b) = load_input(input());
+    println!("{}", similarity_score(a.as_slice(), b.as_slice()));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_parse_line() {
-        assert_eq!(parse_line(""), None);
-        assert_eq!(parse_line("3   4"), Some((3, 4)));
-        assert_eq!(parse_line("35134   63205"), Some((35134, 63205)));
-    }
+    use day01_lib::test_input;
 
     #[test]
-    fn test_load_input() {
-        assert_eq!(
-            load_input(test_input()),
-            (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3])
-        );
-    }
-
-    #[test]
-    fn test_total_distance() {
+    fn test_similarity_score() {
         let (a, b) = load_input(test_input());
-        assert_eq!(total_distance(a, b), 11);
-    }
-
-    fn test_input() -> &'static Path {
-        Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/test_input.txt"))
+        assert_eq!(similarity_score(a.as_slice(), b.as_slice()), 31);
     }
 }
---
+++ day01_lib/src/lib.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
+
+fn parse_line(line: &str) -> Option<(u32, u32)> {
+    let mut it = line.split_whitespace();
+    Some((it.next()?.parse().ok()?, it.next()?.parse().ok()?))
+}
+
+pub fn load_input(path: impl AsRef<Path>) -> (Vec<u32>, Vec<u32>) {
+    let file = BufReader::new(File::open(path).unwrap());
+    file.lines()
+        .map_while(Result::ok)
+        .filter(|line| !line.is_empty())
+        .flat_map(|line| parse_line(line.as_str()))
+        .collect()
+}
+
+pub fn input() -> PathBuf {
+    let mut path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .to_path_buf();
+    path.push("data");
+    path.push("day01");
+    path.push("input.txt");
+    path
+}
+
+pub fn test_input() -> PathBuf {
+    let mut path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .to_path_buf();
+    path.push("test_data");
+    path.push("day01");
+    path.push("test_input.txt");
+    path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(parse_line(""), None);
+        assert_eq!(parse_line("3   4"), Some((3, 4)));
+        assert_eq!(parse_line("35134   63205"), Some((35134, 63205)));
+    }
+
+    #[test]
+    fn test_load_input() {
+        assert_eq!(
+            load_input(test_input()),
+            (vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3])
+        );
+    }
+}
